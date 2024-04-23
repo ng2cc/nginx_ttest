aff5700c6da0851fd749976fbad178e3756ace3e
Jaehyuk-Lee <alex00728@outlook.com>
2024-04-23 15:21:35 +0900

    add HTTP status info

--- html/scripts.js
+++ html/scripts.js
@@ -1,28 +1,32 @@
-function send() {
-    let url = `http://${WAS_IP}:3000/`;
-    let name = document.querySelector("input[name='name']").value;
-    sendGet(url, name);
+function send(type) {
+  const SERVER_IP = `http://${WAS_IP}:3000/`;
+  switch (type) {
+    case 'search':
+      let name = document.querySelector("input[name='name']").value;
+      sendGet(SERVER_IP + type, name);
+      break;
+  }
 }
 
-function sendGet(url, name) {
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = () => {
-        if (http.readyState === XMLHttpRequest.DONE) {
-            if (http.status === 200) {
-                var result = http.response;
-                alert(result);
-            } else {
-                alert('뭔가 문제가 있어요.');
-            }
-        }
+function sendGet(url, name = '') {
+  var http = new XMLHttpRequest();
+  http.onreadystatechange = () => {
+    if (http.readyState === XMLHttpRequest.DONE) {
+      if (http.status === 200) {
+        var result = http.response;
+        alert(result);
+      } else {
+        alert('뭔가 문제가 있어요.\nHTTP Status Code: ' + http.status);
+      }
     }
-    http.open("GET", url + "?name=" + name);
-    http.setRequestHeader("Content-type", "multipart/form-data");
-    http.send();
+  }
+  http.open("GET", url + "?name=" + name);
+  http.setRequestHeader("Content-type", "multipart/form-data");
+  http.send();
 }
 
 addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-        send();
-    }
+  if (e.key === "Enter") {
+    send();
+  }
 });
